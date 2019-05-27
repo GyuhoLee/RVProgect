20890fa9b67a6c7b228ae16f9ae70ac4de7ed861
GyuhoLee <gyuho965@naver.com>
2019-05-27 23:16:33 +0900

    ver_0.11 store file Android에서의 path 적용(asset bundle 사용)

--- RVProject/Assets/Script/ControlItem.cs
+++ RVProject/Assets/Script/ControlItem.cs
@@ -30,29 +30,31 @@ public class ControlItem : MonoBehaviour
 {
     public GameObject ItemObject;
     public Transform Content;
-    
+
     void Start()
     {
         MakeItem();
     }
-    
+
     void MakeItem()
     {
         GameObject GOTemp;
         ItemObject itemObjectTemp;
-        FileStream fs = new FileStream(@"Assets\Resources\File\BallSkin.txt", FileMode.Open, FileAccess.Read);
+        string path;
+#if (UNITY_EDITOR || UNITY_STANDALONE_WIN)
+        path = @"Assets\StreamingAssets\BallSkin.txt";
+        FileStream fs = new FileStream(@"Assets\StreamingAssets\BallSkin.txt", FileMode.Open, FileAccess.Read);
         StreamReader sr = new StreamReader(fs);
         string srTemp = sr.ReadLine();
-        while(srTemp != null)
+        while (srTemp != null)
         {
             string[] values = srTemp.Split(' ');
             GOTemp = Instantiate(this.ItemObject) as GameObject;
             itemObjectTemp = GOTemp.GetComponent<ItemObject>();
             itemObjectTemp.Photo.sprite = Resources.Load<Sprite>("Image/BallSkin/" + values[0]);
-            Debug.Log(values[0]);
             itemObjectTemp.Pay.text = values[1] + "P";
             itemObjectTemp.Name.text = values[2];
-            if(values[3] == "1")
+            if (values[3] == "1")
             {
                 itemObjectTemp.Buy.interactable = false;
             }
@@ -62,8 +64,32 @@ public class ControlItem : MonoBehaviour
             }
             itemObjectTemp.transform.SetParent(this.Content);
             itemObjectTemp.transform.localScale = new Vector3(1, 1, 1);
-
             srTemp = sr.ReadLine();
         }
+#elif UNITY_ANDROID
+        path = "jar:file://" + Application.dataPath + "!/assets/BallSkin.txt";
+        GameObject fTemp = GameObject.Find("FileManager");
+        string srTemp = fTemp.GetComponent<FileManagement>().TextReadInAndroid(path);
+        string[] values = srTemp.Split(' ', '\n');
+        for(int i = 0; i < values.Length - 3; i = i + 4)
+        {
+            GOTemp = Instantiate(this.ItemObject) as GameObject;
+            itemObjectTemp = GOTemp.GetComponent<ItemObject>();
+            itemObjectTemp.Photo.sprite = Resources.Load<Sprite>("Image/BallSkin/" + values[i]);
+            itemObjectTemp.Pay.text = values[i + 1] + "P";
+            itemObjectTemp.Name.text = values[i + 2];
+            if (values[i + 3] == "1")
+            {
+                itemObjectTemp.Buy.interactable = false;
+            }
+            else
+            {
+                itemObjectTemp.Equip.interactable = false;
+            }
+            itemObjectTemp.transform.SetParent(this.Content);
+            itemObjectTemp.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+#endif
     }
 }
---
+++ RVProject/Assets/Script/FileManagement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class FileManagement : MonoBehaviour
+{
+    public string TextReadInAndroid(string path)
+    {
+        string jsonString;
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            WWW reader = new WWW(path);
+            while (!reader.isDone) { }
+            jsonString = reader.text;
+        }
+        else
+        {
+            jsonString = File.ReadAllText(path);
+        }
+        return jsonString;
+    }
+
+    public void writeStringToFile(string str, string filename)
+    {
+     #if !WEB_BUILD
+        string path = pathForDocumentsFile(filename);
+        FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write);
+        StreamWriter sw = new StreamWriter(file);
+        sw.WriteLine(str);
+        sw.Close();
+        file.Close();
+    #endif
+    }
+
+    public string readStringFromFile(string filename)
+    {
+    #if !WEB_BUILD
+        string path = pathForDocumentsFile(filename);
+        if(File.Exists(path))
+        {
+            FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(file);
+            string str = null;
+            str = sr.ReadLine();
+            if (str == null)
+            {
+                sr.Close();
+                file.Close();
+            }
+            return str;
+        }
+        else
+        {
+            return null;
+        }
+    #else
+            return null;
+    #endif
+    }
+
+    public string pathForDocumentsFile(string filename)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            string path = Application.persistentDataPath;
+            path = path.Substring(0, path.LastIndexOf('/'));
+            return Path.Combine(path, filename);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+}
